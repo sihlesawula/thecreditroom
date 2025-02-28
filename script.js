e72a8fb026042dbf3050b787dce08d39384108d0
sihlesawula <150780503+sihlesawula@users.noreply.github.com>
2025-02-28 18:42:35 +0200

    changes

---
+++ script.js
@@ -0,0 +1,6 @@
+// Function to toggle the navigation menu in mobile view
+function toggleMenu() {
+    const links = document.querySelector('.navbar-links');
+    links.classList.toggle('active');
+  }
+  
