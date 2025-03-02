b7b2086b5c901551fb0ba63812cdb05474cae535
sihlesawula <150780503+sihlesawula@users.noreply.github.com>
2025-03-02 17:40:39 +0200

    remove

--- script.js
+++ script.js
@@ -3,23 +3,4 @@ function toggleMenu() {
     const links = document.querySelector('.navbar-links');
     links.classList.toggle('active');
   }
-
-
-
-  window.onload = function() {
-    const popup = document.getElementById('popup');
-    const close = document.getElementById('close');
-    
-    // Check if the user has closed the popup previously
-    if (!localStorage.getItem('popupClosed')) {
-        popup.style.display = 'flex'; // Show popup if not closed
-    }
-
-    // Add event listener to close the popup when X is clicked
-    close.onclick = function() {
-        popup.style.display = 'none';
-        localStorage.setItem('popupClosed', 'true'); // Mark as closed in local storage
-    }
-};
-
   
